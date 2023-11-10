5843c80981df13698bd2cc067bcc09cb447617e4
David Liebherr <ambition.aidev@gmail.com>
2023-11-09 19:59:33 -0600

    WIP, built querys

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,56 +3,27 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
- 
+    users: async () => {
+      const users = await User.find({})
+      return users
+    },
+    user: async (parent, { userId }, context, info) => {
+      const user = User.findById(userId)
+      return user
+    },
+    events: async () => {
+      const events = await Event.find()
+      return events
+    },
+    event: async (parent, { eventId }, context, info) => {
+      const event = await Event.findById(eventId)
+      return event
+    }
 
   },
 
   Mutation: {
-    addProfile: async (parent, { name, email, password }) => {
-      const profile = await Profile.create({ name, email, password });
-      const token = signToken(profile);
-
-      return { token, profile };
-    },
-    login: async (parent, { email, password }) => {
-      const profile = await Profile.findOne({ email });
-
-      if (!profile) {
-        throw AuthenticationError;
-      }
-
-      const correctPw = await profile.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw AuthenticationError;
-      }
-
-      const token = signToken(profile);
-      return { token, profile };
-    },
-
-    addSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        {
-          $addToSet: { skills: skill },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeProfile: async (parent, { profileId }) => {
-      return Profile.findOneAndDelete({ _id: profileId });
-    },
-    removeSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        { $pull: { skills: skill } },
-        { new: true }
-      );
-    },
+   
   },
 };
 
