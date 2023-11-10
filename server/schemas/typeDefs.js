98f1df75648d0c39d3bc2600b7c3178a51e15cac
David Liebherr <ambition.aidev@gmail.com>
2023-11-09 19:04:58 -0600

    WIP, typeDefs mockup

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,28 @@
 const typeDefs = `
   type User {
     _id: ID
-    name: String
-    email: String
-    password: String
+    username: String!
+    email: String!
+    password: String!
     friends: [User]
     events: [Event]
+  }
+
+  input EventInput {
+    eventName: String!
+    location: String
+    description: String
+    startTime: Date!
+    endTime: Date!
+  }
+
+  type Event {
+    _id: ID
+    eventName: String!
+    location: String
+    description: String
+    startTime: Date!
+    endTime: Date!
   }
 
   type Auth {
@@ -16,13 +33,24 @@ const typeDefs = `
   type Query {
     users: [User]!
     user(userId: ID!): User
+    events: [Event]!
+    event(eventId: ID!): Event
   }
 
   type Mutation {
-    addUser(name: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
     removeUser(userId: ID!): User
+
+    addFriend(friendId: ID, userId: ID): User
+    deleteFriend(friendId: ID, userId: ID): User
+
+    updateEvent(eventID: ID, eventInput: EventInput): Event
+    deleteEvent(eventID: ID): Event
+
+
+    addEvent(eventInput: EventInput): Event
+   
   }
 `;
 
